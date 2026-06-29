a59d3cf38a0247fe48d906a386623caf08cf9050
pooja3122005 <poojaumanath3@gmail.com>
2026-06-29 23:55:26 +0530

    Time: 10 ms (8.52%) | Memory: 45.2 MB (25.76%) - LeetSync

--- day1/349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ day1/349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,17 +1,36 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet <Integer> set = new HashSet<>();
-        HashSet <Integer> res = new HashSet<>();
-        for(int num:nums2){
-            if(!set.contains(num))
-               set.add(num);
-        }
-        for(int nums : nums1)
-        {
-            if(set.contains(nums))
-             res.add(nums);
-        }
-        int[] final_res = res.stream().mapToInt(i->i).toArray();
-        return final_res;
+        // HashSet <Integer> set = new HashSet<>();
+        // HashSet <Integer> res = new HashSet<>();
+        // for(int num:nums2){
+        //     if(!set.contains(num))
+        //        set.add(num);
+        // }
+        // for(int nums : nums1)
+        // {
+        //     if(set.contains(nums))
+        //      res.add(nums);
+        // }
+        // int[] final_res = res.stream().mapToInt(i->i).toArray();
+        // return final_res;
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        HashSet<Integer> set= new HashSet<>();
+    	int index1=0;
+    	int index2=0;
+    	while(index1<nums1.length && index2<nums2.length ) {
+    		if(nums1[index1]==nums2[index2]) {
+    			 set.add(nums2[index2]);
+    			index2++;
+    			index1++;
+    		}
+    		else if(nums1[index1] <nums2[index2])
+    		index1++;
+    		else
+    			index2++;
+    	}
+    	return Arrays.stream(set.toArray(new Integer[0])).mapToInt(Integer::intValue).toArray();
+    
     }
 }
