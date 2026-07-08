dedcd0652941c09aed9a89e095e0713eec64ef5e
pooja3122005 <poojaumanath3@gmail.com>
2026-07-08 23:10:22 +0530

    Time: 7 ms (73.76%) | Memory: 49.6 MB (59.61%) - LeetSync

--- day1/49-group-anagrams/group-anagrams.java
+++ day1/49-group-anagrams/group-anagrams.java
@@ -1,17 +1,37 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap <String , List<String>> map = new HashMap<>();
-        for(int i=0;i<strs.length;i++){
-            char [] char_String = strs[i].toCharArray();
-            Arrays.sort(char_String);
-            String key_word = new String(char_String);
-            map.computeIfAbsent(key_word,k-> new ArrayList<String>()).add(strs[i]);
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     HashMap <String , List<String>> map = new HashMap<>();
+    //     for(int i=0;i<strs.length;i++){
+    //         char [] char_String = strs[i].toCharArray();
+    //         Arrays.sort(char_String);
+    //         String key_word = new String(char_String);
+    //         map.computeIfAbsent(key_word,k-> new ArrayList<String>()).add(strs[i]);
 
 
-        }
-        return new ArrayList<>(map.values());    
-        }
-        
+    //     }
+    //     return new ArrayList<>(map.values());    
+    //     }
+
+
+
+            public List<List<String>> groupAnagrams(String[] strs) {
+                        HashMap <String , List<String>> map = new HashMap<>();
+                                for(int i=0;i<strs.length;i++){
+                                    char[] char_String = strs[i].toCharArray();
+                                    Arrays.sort(char_String);
+                                    String keyWord = new String(char_String);
+                                     if(!map.containsKey(keyWord)){
+                                        map.put(keyWord,new ArrayList<String>());
+                                     }
+                                     map.get(keyWord).add(strs[i]);
+                                }
+
+                return new ArrayList<>(map.values());
+
+
+                
+            }
+
 }
 
 // eat
